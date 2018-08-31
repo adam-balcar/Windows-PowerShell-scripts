fca57688b46a2ad07a2af652bcf1d0d6050383fc
Adam Balcar <42859968+adam-balcar@users.noreply.github.com>
2018-08-31 11:02:50 +0200

    Update ADphoneBookPBX.sql

--- ADphoneBookPBX.sql
+++ ADphoneBookPBX.sql
@@ -2,8 +2,8 @@
 SELECT
 
  (CASE 
-			WHEN tblADSI.department like '%BudÏjovice%' THEN REPLACE((cast(SUBSTRING(tblADSI.department,12,20) as varchar(max))collate SQL_Latin1_General_Cp1251_CS_AS),'Ceske ','') + ' - '
-			WHEN tblADSI.department like '%Kr·lovÈ%' THEN REPLACE((cast(SUBSTRING(tblADSI.department,12,20) as varchar(max))collate SQL_Latin1_General_Cp1251_CS_AS),' Kralove','') + ' - '
+			WHEN tblADSI.department like '%Bud√¨jovice%' THEN REPLACE((cast(SUBSTRING(tblADSI.department,12,20) as varchar(max))collate SQL_Latin1_General_Cp1251_CS_AS),'Ceske ','') + ' - '
+			WHEN tblADSI.department like '%Kr√°lov√©%' THEN REPLACE((cast(SUBSTRING(tblADSI.department,12,20) as varchar(max))collate SQL_Latin1_General_Cp1251_CS_AS),' Kralove','') + ' - '
 			WHEN tblADSI.department like '%Labem%' THEN REPLACE((cast(SUBSTRING(tblADSI.department,12,20) as varchar(max))collate SQL_Latin1_General_Cp1251_CS_AS),' nad Labem','') + ' - '
 			WHEN tblADSI.department like 'Expozitura%' THEN (cast(SUBSTRING(tblADSI.department,12,20) as varchar(max))collate SQL_Latin1_General_Cp1251_CS_AS) + ' - '
 			ELSE ''
@@ -22,11 +22,11 @@ UPPER(
 		
 
 
- (CASE WHEN tblADSI.telephoneNumber = '+420 267 990 711' OR
-                      tblADSI.telephoneNumber NOT LIKE '+420 267 990 ___' THEN tblADSI.homePhone ELSE RIGHT(tblADSI.telephoneNumber, 3) END) AS Telephone
+ (CASE WHEN tblADSI.telephoneNumber = '+420 999 990 611' OR
+                      tblADSI.telephoneNumber NOT LIKE '+420 999 990 ___' THEN tblADSI.homePhone ELSE RIGHT(tblADSI.telephoneNumber, 3) END) AS Telephone
 FROM         OPENQUERY(ADSI, 
                       'SELECT  sAMAccountName, homePhone, telephoneNumber, displayName, department
-  FROM  ''LDAP://das.local/OU=ZamÏstnanci,OU=Firma,DC=das,DC=local'' 
+  FROM  ''LDAP://company.local/OU=Zam√¨stnanci,OU=Firma,DC=company,DC=local'' 
   WHERE objectClass =  ''User'' 
   ') 
                       AS tblADSI
@@ -51,7 +51,7 @@ UPPER(
 
 FROM         OPENQUERY(ADSI, 
                       'SELECT  sAMAccountName, mobile, displayName 
-  FROM  ''LDAP://das.local/OU=ZamÏstnanci,OU=Firma,DC=das,DC=local'' 
+  FROM  ''LDAP://company.local/OU=Zam√¨stnanci,OU=Firma,DC=company,DC=local'' 
   WHERE objectClass =  ''User'' 
   ') 
                       AS tblADSI
