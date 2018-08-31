7cad24dce4e9c41670f2e8a70173b73ea835ed57
Adam Balcar <42859968+adam-balcar@users.noreply.github.com>
2018-08-31 10:13:38 +0200

    Add files via upload

---
+++ DeleteUser.ps1
@@ -0,0 +1,112 @@
+﻿#DELETE USER
+
+# User info
+$login="novak"
+# Email redirection to
+$presmerovani="info@company.cz"
+# AD container for removed users
+$kontejner = '2018-07'
+
+
+# Create credential $cred
+$user = "$Env:UserName"
+$passwdFile = "$env:USERPROFILE\MyScript-$user"
+if ((Test-Path $passwdFile) -eq $false) {
+  $cred = new-object system.management.automation.pscredential $user,
+        (read-host -assecurestring -prompt "Enter $user password")
+    $cred.Password | ConvertFrom-SecureString | Set-Content $passwdFile
+}
+else {
+  $cred = new-object system.management.automation.pscredential $user,
+        (Get-Content $passwdFile | ConvertTo-SecureString)
+}
+
+# Import AD module
+Try 
+{ 
+  Import-Module ActiveDirectory -ErrorAction Stop 
+} 
+Catch 
+{ 
+  Write-Host "[ERROR] ActiveDirectory Module couldn't be loaded. Script will stop!" 
+  Exit 1 
+} 
+
+# Import Exchange module
+Try 
+{  
+    $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://Email/PowerShell/ -Authentication Kerberos -Credential $cred
+    Import-PSSession $session -AllowClobber -ErrorAction Stop
+} 
+Catch 
+{ 
+  Write-Host "[ERROR] Exchange Module couldn't be loaded. Script will stop!" 
+  Exit 1 
+}
+
+# Values formatting
+$vyraz = "OU=$kontejner,OU=Vyřazené účty,OU=Firma,DC=company,DC=local"
+$email = $login + "@company.cz"
+$name = Get-ADUser -Identity $login | Select -Expand Name
+
+
+# Create admin credential $creda
+$user = "Administrator@company.local"
+$passwdFile = "$env:USERPROFILE\MyScript-$user"
+if ((Test-Path $passwdFile) -eq $false) {
+  $creda = new-object system.management.automation.pscredential $user,
+        (read-host -assecurestring -prompt "Enter Administrator password")
+    $creda.Password | ConvertFrom-SecureString | Set-Content $passwdFile
+}
+else {
+  $creda = new-object system.management.automation.pscredential $user,
+        (Get-Content $passwdFile | ConvertTo-SecureString)
+}
+
+# import Azure AD module
+# Create credential user@company.cz $credo
+$usermail = "$Env:UserName@company.cz"
+$passwdFile = "$env:USERPROFILE\MyScript-$usermail"
+if ((Test-Path $passwdFile) -eq $false) {
+  $credo = new-object system.management.automation.pscredential $usermail,
+        (read-host -assecurestring -prompt "Enter $usermail password")
+    $credo.Password | ConvertFrom-SecureString | Set-Content $passwdFile
+}
+else {
+  $credo = new-object system.management.automation.pscredential $usermail,
+        (Get-Content $passwdFile | ConvertTo-SecureString)
+} 
+
+# connect Azure AD module
+Connect-MsolService -credential $credo
+$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $credo -Authentication Basic -AllowRedirection
+Import-PSSession $session -AllowClobber
+
+
+#Setting O365 - redirection, shared mailobox, removie licence, move in AD
+Set-Mailbox -Identity $email -ForwardingAddress $presmerovani
+Set-Mailbox -Identity $email -Type shared 
+
+Write "300s pauza for sync"
+Start-Sleep -s 300
+
+Set-MsolUserLicense -UserPrincipalName $email -RemoveLicenses "dascz:STANDARDPACK" 
+
+$cestausr = (get-aduser -f {mailNickname -eq $login}).DistinguishedName
+Move-ADObject -Identity $cestausr -TargetPath $vyraz
+
+
+$groups = (Get-ADPrincipalGroupMembership $login).name
+Foreach ($group in $groups) {
+if($group -ne "Domain Users"){remove-adgroupmember -Identity $group -Member $login -Confirm:$false }
+}
+
+Disable-ADAccount -Identity $login
+
+$zobrjm = 'Ω '+$name
+$ajdent = "company.local/Firma/Vyřazené účty/$kontejner/$name"
+
+
+Set-RemoteMailbox -DisplayName $zobrjm -HiddenFromAddressListsEnabled $true -Identity $ajdent
+
+write-host "Finished."
---
+++ ResetPassword.ps1
@@ -0,0 +1,42 @@
+﻿
+# Import-Module AD + SQL 
+Import-Module ActiveDirectory, SqlServer
+
+# Enter user login here
+$login="jan.novak"
+
+# Random 8 characters password generator, with user friendly vowels and consonants.
+$h1 = Get-Random -InputObject "A","E","I","O","U"
+$h2 = Get-Random -InputObject "b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x"
+$h3 = Get-Random -InputObject "a","e","i","o","u"
+$h4 = Get-Random -InputObject "b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x"
+$rnd=Get-Random -Minimum 1000 -Maximum 10000
+$heslo=$h1+$h2+$h3+$h4+$rnd
+
+
+$telefon = (get-aduser $login -Properties mobile).mobile -replace (' ') # Cell number from AD
+if (!$telefon) {$telefon = (get-aduser $login -Properties telephonenumber).telephonenumber -replace (' ')} # No cell = hard line number
+if (!$telefon) {$telefon = $(Read-Host "Zadej mobilní číslo pro zaslání hesla")} # No number = enter manually
+#Write $telefon
+
+# AD > Set new password, unlock account.
+Set-ADAccountPassword $login -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $heslo -Force)
+Unlock-ADAccount -Identity $login
+
+# Text message body
+$sms = "Dobrý den. Vaše nové heslo pro přístup je: $heslo"
+
+# Query for sms gateway
+$query = @”
+
+INSERT INTO [XXX].[dbo].[Messages]
+      (Sender, Recipient, Body)
+VALUES
+      ('+420777666555', '$telefon', '$sms')
+“@
+
+# Run query.
+Invoke-Sqlcmd -ServerInstance SQL -Query $query
+
+# Info msg
+write-host $login " / " $heslo " - SMS odeslána na telefon " $telefon
